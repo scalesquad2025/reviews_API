137de10e619ff10337a09db7688ca0977c15ee33
Lucas <lucascostamonteiro@yahoo.com>
2025-02-21 09:48:01 -0800

    deployed database connected

--- server/database/db.js
+++ server/database/db.js
@@ -3,30 +3,6 @@ require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 const pgp = require('pg-promise')();
 
 // const dbConfig = {
-  // user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: String(process.env.DB_PASSWORD || '')
-//   port: process.env.DB_PORT,
-// };
-
-const dbConfig = {
-  user: 'postgres',
-  host: '54.218.123.241',
-  database: 'reviews_db',
-  password: String(process.env.DB_PASSWORD || ''),
-  port: 5432,
-};
-
-
-const db = pgp(dbConfig);
-
-module.exports = db;
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
-const pgp = require('pg-promise')();
-
-// const dbConfig = {
 // user: process.env.DB_USER,
 //   host: process.env.DB_HOST,
 //   database: process.env.DB_DATABASE,
@@ -36,7 +12,7 @@ const pgp = require('pg-promise')();
 
 const dbConfig = {
   user: 'postgres',
-  host: '172.31.20.98',
+  host: '34.222.11.57',
   database: 'reviews_db',
   password: String(process.env.DB_PASSWORD || ''),
   port: 5432,
@@ -44,5 +20,10 @@ const dbConfig = {
 
 
 const db = pgp(dbConfig);
+
+
+// db.one('SELECT NOW()')
+//   .then(data => console.log('Connected to DB:', data))
+//   .catch(error => console.error('Error connecting:', error));
 
 module.exports = db;
